d6bb0848993379a85b41a9816fe9ebf8822aec74
mjnav <mjnavarro24@yahoo.com>
2019-10-27 02:24:57 -0700

    updates

--- main.py
+++ main.py
@@ -32,18 +32,14 @@ def get_file():
     error = None
     # try:
     if request.method == "POST":
-        print(request.files)
         attemptedFile = request.files["fileToUpload"]
-        print('here', attemptedFile)
         filename = secure_filename(attemptedFile.filename)
         newfile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         attemptedFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        #print('this file', filename, 'and the attepmpt', attemptedFile)
         with open(newfile, 'rb') as source_image:
               source_bytes = source_image.read()
         schedule = create_schedule(source_bytes)
         return render_template("modifySchedule.html", schedule = schedule)
-        #uncomment this later pls return render_template("modifySchedule", schedule = schedule)
     # except Exception as e:
     #     flash(e)
     #
