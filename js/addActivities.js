7cfdacab3e49113a33ca7a2c71ff0c27a440d70a
israeljimenez <israelj@uci.edu>
2019-10-27 10:05:28 -0700

    tweaked visuals

--- js/addActivities.js
+++ js/addActivities.js
@@ -2,37 +2,48 @@ let activities = []
 add = document.getElementById('add');
 
 class Activity{
-    constructor(activity,hour, mins){
+    constructor(element,activity,hour, mins){
+        this.element = element;
         this.activity = activity;
         this.length = new TimeLength(hour,mins);
     }
+
 }
 
 class TimeLength{
     constructor(hour, mins){
         this.hour = hour;
         this.mins = mins;
-        
     }
 }
 
 add.addEventListener('click', () => {
-    let hour = parseInt(document.getElementById("selectHour").value);
+    let hourE = parseInt(document.getElementById("selectHour").value);
+    let hour = hour;
     let mins = document.getElementsByClassName("selectMins");
-    let min = parseInt(mins[0].value)*10+ parseInt(mins[1].value)
+    let min = parseInt(mins[0].value)*10+ parseInt(mins[1].value);
     let activity = document.getElementById('activity').value;
     let list = document.getElementById('activities');
-    console.log(activity);
+    let errorActivity = document.getElementById('error-activity');
+
+    if (hour != NaN || min != NaN || activity != "" || activities.length == 0){
+        errorActivity.style.display = 'block';
+    }
 
     if (hour != NaN && min != NaN && activity != "" && activities.length == 0){
-        activities.push(new Activity(activity, new TimeLength(hour, min)));
-        list.innerHTML = `<li class="list-group-item"><span>`+activity+`</span>;
+        errorActivity.style.display = 'none';
+        list.innerHTML = `<li class="list-group-item"><span>`+activity+`</span>
                         <span style="float: right;">`+ hour+`:`+min+`</span></li>`;
+        let lists = list.getElementsByClassName('list-group-item');
+        activities.push(new Activity(lists[0],activity, new TimeLength(hour, min)));
+        clearForm(activity, hour, )
     }
     else if(hour != NaN && min != NaN && activity != "" && activities.length > 0){
-        activities.push(new Activity(activity, new TimeLength(hour, min)));
+        errorActivity.style.display = 'none';
         list.innerHTML += `<li class="list-group-item"><span class="hour">`+activity+`</span>
                         <span class="min" style="float: right;">`+hour+`:`+min+`</span></li>`;
+        let lists = list.getElementsByClassName('list-group-item');
+        activities.push(new Activity(lists[lists.length-1],activity, new TimeLength(hour, min)));
     }
     if (activities.length == 5){
         list.style.overflow = 'hidden';
@@ -40,3 +51,7 @@ add.addEventListener('click', () => {
         list.style.height = '200px';
     }
 });
+
+function clearForm(activity, hour, min){
+
+}
